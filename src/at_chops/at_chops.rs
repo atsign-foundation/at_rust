7f2afa03e2d0d3b0581e5b7043276e43bd0db2ff
Doug Todd <doug.todd@hey.com>
2023-09-02 21:52:58 +0100

    chore: Removed unwanted print statement and comment

--- src/at_chops/at_chops.rs
+++ src/at_chops/at_chops.rs
@@ -70,7 +70,6 @@ pub fn encrypt_data_with_shared_symmetric_key(encoded_symmetric_key: &str, data:
     let decoded_symmetric_key = base64_decode(&encoded_symmetric_key);
     let iv: [u8; 16] = [0x00; 16];
     let mut cypher = construct_aes_key(&decoded_symmetric_key, &iv);
-    // TODO: Need to pad this for Pkcs7
     let encrypted_data = encrypt_data_with_aes_key(&mut cypher, &data.as_bytes());
     base64_encode(&encrypted_data)
 }
@@ -78,7 +77,6 @@ pub fn encrypt_data_with_shared_symmetric_key(encoded_symmetric_key: &str, data:
 /// Decrypt data with an encoded AES symm key.
 pub fn decrypt_data_with_shared_symmetric_key(encoded_symmetric_key: &str, data: &str) -> String {
     let decoded_symmetric_key = base64_decode(&encoded_symmetric_key);
-    info!("Symmetric key: {:x?}", decoded_symmetric_key);
     let iv: [u8; 16] = [0x00; 16];
     let mut cypher = construct_aes_key(&decoded_symmetric_key, &iv);
     let decoded_data = base64_decode(&data);
